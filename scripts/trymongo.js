6cc67f3cfa8a198540b8cec4c235f29270d76845
Leona-qy <82625569+Leona-qy@users.noreply.github.com>
2022-03-20 18:51:18 +0800

    Update trymongo.js

--- scripts/trymongo.js
+++ scripts/trymongo.js
@@ -1,65 +1,45 @@
 const { MongoClient } = require('mongodb');
 
-const url = 'mongodb://localhost/issuetracker';
+const url = 'mongodb://localhost/traveler';
 
-// Atlas URL  - replace UUU with user, PPP with password, XXX with hostname
-// const url = 'mongodb+srv://UUU:PPP@cluster0-XXX.mongodb.net/issuetracker?retryWrites=true';
-
-// mLab URL - replace UUU with user, PPP with password, XXX with hostname
-// const url = 'mongodb://UUU:PPP@XXX.mlab.com:33533/issuetracker';
-
-function testWithCallbacks(callback) {
-  console.log('\n--- testWithCallbacks ---');
-  const client = new MongoClient(url, { useNewUrlParser: true });
-  client.connect(function(err, client) {
-    if (err) {
-      callback(err);
-      return;
-    }
-    console.log('Connected to MongoDB');
-
-    const db = client.db();
-    const collection = db.collection('employees');
-
-    const employee = { id: 1, name: 'A. Callback', age: 23 };
-    collection.insertOne(employee, function(err, result) {
-      if (err) {
-        client.close();
-        callback(err);
-        return;
-      }
-      console.log('Result of insert:\n', result.insertedId);
-      collection.find({ _id: result.insertedId})
-        .toArray(function(err, docs) {
-        if (err) {
-          client.close();
-          callback(err);
-          return;
-        }
-        console.log('Result of find:\n', docs);
-        client.close();
-        callback(err);
-      });
-    });
-  });
-}
-
-async function testWithAsync() {
-  console.log('\n--- testWithAsync ---');
+async function testCRUD() {
+  console.log('\n--- testCRUD ---');
   const client = new MongoClient(url, { useNewUrlParser: true });
   try {
     await client.connect();
     console.log('Connected to MongoDB');
     const db = client.db();
-    const collection = db.collection('employees');
+    const collection = db.collection('travellers');
 
-    const employee = { id: 2, name: 'B. Async', age: 16 };
-    const result = await collection.insertOne(employee);
-    console.log('Result of insert:\n', result.insertedId);
+    //test add logic
+    const traveller1 = { id: 1, name: 'A. Test', phonenum: 12345678 };
+    const traveller2 = { id: 2, name: 'B. Test', phonenum: 88889999 };
+    const result1 = await collection.insertOne(traveller1);
+    const result2 = await collection.insertOne(traveller2);
+    console.log('Result of insert:\n', result1.insertedId, result2.insertedId);
 
-    const docs = await collection.find({ _id: result.insertedId })
+    //test find logic
+    const docs = await collection.find({})
       .toArray();
     console.log('Result of find:\n', docs);
+
+    //test update logic
+
+    const result3 = await collection.updateOne({id: 2}, {$set: {phonenum: 86887677} });
+    console.log('Result of update:\n', result2.insertedId);
+
+    const docs1 = await collection.find({})
+      .toArray();
+    console.log('Result of find:\n', docs1);
+
+    //test delete logic
+    const result4 = await collection.deleteOne({id: 2});
+    console.log('Result of delete:\n', result2.insertedId);
+
+    const docs2 = await collection.find({})
+      .toArray();
+    console.log('Result of find:\n', docs2);
+
   } catch(err) {
     console.log(err);
   } finally {
@@ -67,9 +47,5 @@ async function testWithAsync() {
   }
 }
 
-testWithCallbacks(function(err) {
-  if (err) {
-    console.log(err);
-  }
-  testWithAsync();
-});
+
+testCRUD()
